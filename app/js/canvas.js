6402f85bcddbd07c74b679ff982a7897f7163ef4
aditore <anthonyditore@gmail.com>
2023-04-02 09:21:16 -0700

    first commit, looking into canvas in html

---
+++ app/js/canvas.js
@@ -0,0 +1,10 @@
+/* Creating CANVAS for HTML */
+
+var canvas = document.getElementById('gameCanvas');
+var ctx = canvas.getContext("2d");
+
+    
+ctx.fillStyle = "#000000";
+ctx.fillRect(0, 0, 500, 500);
+
+

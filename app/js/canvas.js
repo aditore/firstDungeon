535f3c31510685a35162c39b6df202021c66c3a4
aditore <anthonyditore@gmail.com>
2023-04-03 22:11:59 -0700

    finished, will update with comments, learned alot!

--- app/js/canvas.js
+++ app/js/canvas.js
@@ -20,20 +20,60 @@ colorInput.value = "#009578";
 ctx.fillStyle = "#FFFFFF";
 ctx.fillRect(0, 0, canvas.width, canvas.height); 
 
-function handleCanvasMousedown(e) {
+// Setup guide
+{
+    guide.style.width = `${canvas.width}px`;
+    guide.style.height = `${canvas.height}px`;
+    guide.style.gridTemplateColumns = `repeat(${CELL_SIDE_COUNT}, 1fr)`;
+    guide.style.gridTemplateRows = `repeat(${CELL_SIDE_COUNT}, 1fr)`;
 
+    [...Array(CELL_SIDE_COUNT ** 2)].forEach(() => guide.insertAdjacentHTML("beforeend", "<div></div>"));
+}
+
+function handleCanvasMousedown(e) {
+    // Ensure user is using their primary mouse button
+    if (e.button !== 0) {
+        return;
+    }
+
+    const canvasBoundingRect = canvas.getBoundingClientRect();
+    const x = e.clientX - canvasBoundingRect.left;
+    const y = e.clientY -canvasBoundingRect.top;
+    const cellX = Math.floor(x / cellPixelLength);
+    const cellY = Math.floor(y / cellPixelLength);
+
+    const currentColor = colorHistory[`${cellX}_${cellY}`];
+    if(e.ctrlKey) {
+        if (currentColor) {
+            colorInput.value = currentColor;
+        }
+    }
+
+    fillCell(cellX, cellY);
 }
 
 function handleClearButtonClick() {
+    const yes = confirm("CLEAR CANVAS?");
 
+    if(!yes) {
+        return;
+    }
+
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function handleToggleGuideChange() {
-
+    guide.style.display = toggleGuide.checked ? null : "none";
 }
 
 function fillCell(cellX, cellY) {
+    const startX = cellX * cellPixelLength;
+    const startY = cellY * cellPixelLength;
 
+    ctx.fillStyle = colorInput.value;
+    ctx.fillRect(startX, startY, cellPixelLength, cellPixelLength);
+    colorHistory[`${cellX}_${cellY}`] = colorInput.value;
 }
 
 canvas.addEventListener("mousedown", handleCanvasMousedown);

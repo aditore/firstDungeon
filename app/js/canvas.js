7b6e494cf4d349f2afecb7b5556aa4e0c7e01196
aditore <anthonyditore@gmail.com>
2023-04-03 21:37:49 -0700

    starting with html and defineing constants in js

--- app/js/canvas.js
+++ app/js/canvas.js
@@ -1,1 +1,7 @@
 /* Creating CANVAS for HTML */
+
+const canvas = document.getElementById("canvas");
+const guide = document.getElementById("guide");
+const colorInput = document.getElementById("colorInput");
+const toggleGuide = document.getElementById("toggleGuide");
+const clearButton = document.getElementById("clearButton");
